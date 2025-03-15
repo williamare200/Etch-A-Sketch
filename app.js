a7fb5f0859e2f2abfabd07ee78a676ca4fe5abec
william arevalos <williamare200@gmail.com>
2025-03-15 16:53:03 -0300

    feature: function squareGrid and styles square

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+/* query container*/
+const container = document.querySelector(".container");
 
+
+/* function 16x16 square divs */
+function divSquareGrid(size) {
+    for (let index = 0; index < size * size; index++) {
+        const square = document.createElement("div");
+        container.appendChild(square);      
+    }
+}
+
+divSquareGrid(16)
