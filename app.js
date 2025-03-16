da599d072fc766c6c5ffd4840ad5dc75a1a57d8d
william arevalos <williamare200@gmail.com>
2025-03-16 16:43:08 -0300

    feature: reset button and prompt size of new grid

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ let isDrawing = false;
 // container.appendChild(square);
 /* function 16x16 square divs */
 function gridDivSize(size) {
+// clear previous grid
+    container.replaceChildren();
+// dynamically calculate size based on container width
+    const squareSize = container.clientWidth / size;
+
     for (let index = 0; index < size * size; index++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+// set dynamic size for each square
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         // Hover Effect (Persistent when dragging)
      square.addEventListener('mouseover', () => {
-        if (isDrawing) square.style.backgroundColor = 'darkblue';
+        if (isDrawing) square.style.backgroundColor = 'black';
       });
   
       // Click to change color immediately
       square.addEventListener('mousedown', () => {
-        square.style.backgroundColor = 'darkblue';
+        square.style.backgroundColor = 'black';
       });
     container.appendChild(square);
     }
@@ -23,6 +32,11 @@ function gridDivSize(size) {
 }
 container.addEventListener('mousedown', () => (isDrawing = true));
 container.addEventListener('mouseup', () => (isDrawing = false));
-gridDivSize(16)
 
-
+gridDivSize(16);
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let size = prompt("enter size x size of grid(max 100x100)", 16);
+    size = Math.min(100, Math.max(1, size)); // ensure the size is within 1 100
+    gridDivSize(size);
+})
