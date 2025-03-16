a3da5eee6360b021f34e7f8dab40bae5e1f91c68
william arevalos <williamare200@gmail.com>
2025-03-16 16:23:12 -0300

    feature: hover function effect paint blue each div

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 /* query container*/
 const container = document.querySelector(".container");
-
+let isDrawing = false;
 // container.appendChild(square);
 /* function 16x16 square divs */
 function gridDivSize(size) {
     for (let index = 0; index < size * size; index++) {
         const square = document.createElement("div");
-        // create class to focus styles css
         square.classList.add("square");
-        container.appendChild(square);      
+        // Hover Effect (Persistent when dragging)
+     square.addEventListener('mouseover', () => {
+        if (isDrawing) square.style.backgroundColor = 'darkblue';
+      });
+  
+      // Click to change color immediately
+      square.addEventListener('mousedown', () => {
+        square.style.backgroundColor = 'darkblue';
+      });
+    container.appendChild(square);
     }
-}
 
+
+}
+container.addEventListener('mousedown', () => (isDrawing = true));
+container.addEventListener('mouseup', () => (isDrawing = false));
 gridDivSize(16)
+
+
